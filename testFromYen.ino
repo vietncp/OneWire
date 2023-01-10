a71b28a9aedce79c683d74e51a8213b2d8d3b60b
Nguyễn Cảnh Việt <65004145+vietncp@users.noreply.github.com>
2023-01-10 13:41:00 +0700

    Update testFromYen.ino

--- testFromYen.ino
+++ testFromYen.ino
@@ -1,4 +1,8 @@
-#define DS18B20_PIN 2
+/*
+Chương trình này được viết dựa trên giao thức 1-Wire dùng cho việc kết nối DS18B20 và Arduino.
+Nguyễn Cảnh Việt, Khoa Vật lý, Đại học Khoa học Tự nhiên.
+*/
+
 
 void  SetMode(unsigned char mode){//set tín hiệu I/O cho pin
   if (mode==0){
@@ -89,8 +93,8 @@ void setup(){
   // Write Scratch pad
   DS18B20_Write_Byte(0xCC);               //skip ROM
   delay(1000);
+  //Ví dụ về cài đặt độ phân giải cho ADC tích hợp trong DS18B20
   
-  // cai dat do phan giai cho ADC
   DS18B20_Reset();
   DS18B20_Write_Byte(0xCC);               //skip ROM
   DS18B20_Write_Byte(0x4E);               //Write cratchpad
@@ -98,50 +102,11 @@ void setup(){
   DS18B20_Write_Byte(0x00);               //Write first byte(value 0) to byte 2
   DS18B20_Write_Byte(0x00);               //Write second byte(value 0) to byte 3
   DS18B20_Write_Byte(0x5F);               //Write third byte(value 5F) to  CONFIG register(byte 4)
-  // 1F-> 9 bit resolution; 3F-> 10 bit resolution; 5F -> 11 bit resolution;7F->12 bit resolution;
-  
+  // 1F-> 9 bit resolution; 3F-> 10 bit resolution; 5F -> 11 bit resolution;7F->12 bit resolution; 
  
 }
+
 void loop(){
-  unsigned char j;
-  unsigned char lsB, msB;  
-  unsigned char data[8];
-  float t;
+// Các bạn tự viết chương trình thêm vào mục này
   
-  DS18B20_Reset();
-  DS18B20_Write_Byte(0xCC);               //skip ROM
-  DS18B20_Write_Byte(0x44);               //convert_T
-  delay(1000);
-  DS18B20_Reset();
-  DS18B20_Write_Byte(0xCC);                 //skip ROM
-  DS18B20_Write_Byte(0xBE);                 //read Scratchpad
-  Serial.print("Data= ");
-  
-  for(j=0;j<8;j++){
-    data[j]=DS18B20_Read_Byte();              //đọc từng byte trong scratchpad và lưu vào biến data
-    
-  Serial.print(data[j],HEX);
-  Serial.print(".");
-  }
-  
-  //lsB=DS18B20_Read_Byte();              // đọc LS Byte
-  //msB=DS18B20_Read_Byte();              // đọc MS Byte
-
-  Serial.println(" ");
-  Serial.println("Nhiet do: ");
-  t= ((float)(data[0]&0x0F))/16
-  t=((data[0]&0xF0)>>4|(data[1]&0x0F)<<4)+t;  //byte thứ nhất lấy 4 bit cuối>>4 +byte thứ 2 lấy 4 bit đầu<<4+phần thập phân ở trên
-  Serial.println(t,4);                //in ra giá trị nhiệt độ, 4 vị trí cho giá trị thập phân
-  
-  Serial.println("ADC resolution: ");
-  Serial.println(data[4],HEX);      //in ra gia tri o dinh dang so HEX
-  Serial.println(data[4],BIN);      //in ra gia tri o dinh dang so BIN
-           
-  
-  
-  
-  //t= ((float)(lsB&0x0F))/16;              //byte thứ nhất lấy 4 bit đầu (phần thập phân)
-  //t=((lsB&0xF0)>>4|(msB&0x0F)<<4)+t;  //byte thứ nhất lấy 4 bit cuối>>4 +byte thứ 2 lấy 4 bit đầu<<4+phần thập phân ở trên
-  //Serial.println(t,4);                //in ra giá trị nhiệt độ, 4 vị trí cho giá trị thập phân
-
 }
